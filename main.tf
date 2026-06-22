80524ccb72ed42cdd9e793019eaea6a02e0a5703
EC2 Default User <ec2-user@ip-172-31-86-186.ec2.internal>
2026-06-22 08:51:59 +0000

    configured s3 remote backend for state management

--- main.tf
+++ main.tf
@@ -5,12 +5,22 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  # Stores the state file securely in your central S3 bucket
+  backend "s3" {
+    bucket  = "redya-terraform-state-backend"
+    key     = "global/s3/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
 }
 
+# Configures the target AWS cloud deployment region
 provider "aws" {
   region = "us-east-1"
 }
 
+# The actual infrastructure resource Jenkins will deploy and destroy
 resource "aws_s3_bucket" "my_automation_bucket" {
   bucket_prefix = "jenkins-terraform-automation-"
   force_destroy = true
