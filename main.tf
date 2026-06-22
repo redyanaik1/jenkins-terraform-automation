c3ecf289a25372ccebf932d209896b4189e2e064
EC2 Default User <ec2-user@ip-172-31-86-186.ec2.internal>
2026-06-22 08:41:59 +0000

    added complete provider and resource infrastructure definition

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my_automation_bucket" {
+  bucket_prefix = "jenkins-terraform-automation-"
+  force_destroy = true
+}
